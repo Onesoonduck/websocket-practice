7747a4d2b7c454ebcc226ce3baa501c357b6083f
soonduck <soonduck3729@gmail.com>
2024-05-17 15:14:51 +0900

    fix: code refactoring

--- src/main/java/com/springv/elice_websocket/common/exception/OutOfStockException.java
+++ src/main/java/com/springv/elice_websocket/common/exception/OutOfStockException.java
@@ -1,10 +1,14 @@
 package com.springv.elice_websocket.common.exception;
 
 import com.springv.elice_websocket.common.enums.ErrorCode;
+import lombok.Getter;
 
+@Getter
 public class OutOfStockException extends RootException {
 
-    public OutOfStockException(String message) {
+    private ErrorCode errorCode;
+
+    public OutOfStockException() {
         super(ErrorCode.OUT_OF_STOCK.getMessage());
     }
 

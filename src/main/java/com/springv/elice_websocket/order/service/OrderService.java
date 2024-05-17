0d13da6eefa7a40a5ee854c4200b13b7349d4904
soonduck <soonduck3729@gmail.com>
2024-05-17 16:24:14 +0900

    feat: use ApplicationEventPublisher, event-driven alarm logic

--- src/main/java/com/springv/elice_websocket/order/service/OrderService.java
+++ src/main/java/com/springv/elice_websocket/order/service/OrderService.java
@@ -1,6 +1,9 @@
 package com.springv.elice_websocket.order.service;
 
 import com.springv.elice_websocket.book.domain.Book;
+import com.springv.elice_websocket.common.enums.ChatCode;
+import com.springv.elice_websocket.common.enums.ErrorCode;
+import com.springv.elice_websocket.common.event.AlarmEvent;
 import com.springv.elice_websocket.common.exception.OutOfStockException;
 import com.springv.elice_websocket.order.domain.Order;
 import com.springv.elice_websocket.order.model.OrderRequest;
@@ -8,6 +11,7 @@ import com.springv.elice_websocket.order.model.OrderResponse;
 import com.springv.elice_websocket.repository.book.BookRepository;
 import com.springv.elice_websocket.repository.order.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -16,6 +20,8 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final BookRepository bookRepository;
+    // Spring applicationContext에 등록된 빈을 참조할 수 있는 인터페이스
+    private final ApplicationEventPublisher eventPublisher; // 발행
 
     public OrderResponse saveOrder(OrderRequest orderRequest) {
 
@@ -27,6 +33,8 @@ public class OrderService {
 
         //재고 조회
         if (isStockInSufficient(orderRequest.getQuantity(), book.getQuantity())) {
+            // 알림 전송 - 이벤트 드리븐 기반 프로그래밍
+            eventPublisher.publishEvent(new AlarmEvent(ChatCode.ORDER_ISSUE.getChatUuid(), ErrorCode.OUT_OF_STOCK));
             throw new OutOfStockException();
         }
 
